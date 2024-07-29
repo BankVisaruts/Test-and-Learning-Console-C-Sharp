9294bfe28bb2b6aafeac3f9f8e9ed38669f779ef
BankVisaruts <visarut.ik@gmail.com>
2024-07-29 11:19:29 +0700

    Add function while and color

--- TestAndLearningConsoleApp/Program.cs
+++ TestAndLearningConsoleApp/Program.cs
@@ -4,34 +4,47 @@ class Program
 {
     static void Main()
     {
-        //Main
+        // Get the width of the console window
+        int consoleWidth = Console.WindowWidth;
+        string line = new string('-', consoleWidth);
+
+        // Main
         Console.WriteLine("--- Hello Welcome to Test and Learning Console App C# ---");
-        Console.WriteLine("---------------------------------------------------------");
-        Console.WriteLine("Please select function Test and Learning");
+        Console.WriteLine(line);
 
-        //Functon Detail
-        Console.WriteLine("Input '1' : Even numbers, odd numbers");
+        bool keepRunning = true;
 
-        //Function Input
-        Console.WriteLine("---------------------------------------------------------");
-        Console.Write("Input the number of the function you want to select : ");
-        string? option = Console.ReadLine();
-
-        //Functoin Option
-        switch (option)
+        while (keepRunning)
         {
-            case ("1"):
-                Console.Write("Input your number : ");
-                string? number = Console.ReadLine();
-                Functions.funcEvenNumberAndOddNumber(number);
-                break;
-            default:
-                Console.WriteLine("Invalid option. Please try again.");
-                break;
-        }
+            // Function Detail
+            Console.WriteLine("Please select function Test and Learning");
+            Console.WriteLine("Input '1' : Even numbers, odd numbers");
+            Console.WriteLine("Input '0' : Exit");
 
-        // Wait process
-        Console.ReadLine();
+            // Function Input
+            Console.WriteLine(line);
+            Console.Write("Input the number of the function you want to select: ");
+            string? option = Console.ReadLine();
+
+            // Function Option
+            switch (option)
+            {
+                case "1":
+                    Console.Write("Input your number: ");
+                    string? number = Console.ReadLine();
+                    Functions.funcEvenNumberAndOddNumber(number);
+                    break;
+                case "0":
+                    keepRunning = false;
+                    Console.WriteLine("Exiting the program. Goodbye!");
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid option. Please try again.");
+                    Console.ResetColor();
+                    break;
+            }
+        }
     }
 }
 
@@ -39,20 +52,26 @@ public class Functions
 {
     public static void funcEvenNumberAndOddNumber(string? number)
     {
+        //Convert int
         if (int.TryParse(number, out int cvNumber))
         {
             if (cvNumber % 2 == 0)
             {
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("{0} is an Even Number", cvNumber);
             }
             else
             {
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("{0} is an Odd Number", cvNumber);
             }
         }
         else
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Invalid input. Please enter a valid number.");
         }
+
+        Console.ResetColor();
     }
 }
--- TestAndLearningConsoleApp/obj/Debug/net8.0/TestAndLearningConsoleApp.AssemblyInfo.cs
+++ TestAndLearningConsoleApp/obj/Debug/net8.0/TestAndLearningConsoleApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestAndLearningConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b265871d36bae3442e135bafb9949b1b3341200e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3a832de699c133af7adbcf534febbcf9eb18a95")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestAndLearningConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestAndLearningConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
