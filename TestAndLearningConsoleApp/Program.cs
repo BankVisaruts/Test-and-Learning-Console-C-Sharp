3d3e08038c1900123d54e469671b59961ac2eb67
BankVisaruts <visarut.ik@gmail.com>
2024-07-30 20:04:19 +0700

    Add Function Random

--- TestAndLearningConsoleApp/Program.cs
+++ TestAndLearningConsoleApp/Program.cs
@@ -19,11 +19,12 @@ class Program
             // Function Detail
             Console.WriteLine("Please select function Test and Learning");
             Console.WriteLine("Input '1' : Even numbers, odd numbers");
+            Console.WriteLine("Input '2' : Random Number");
             Console.WriteLine("Input 'exit' : Exit");
 
             // Function Input
             Console.WriteLine(line);
-            Console.Write("Input the number of the function you want to select: ");
+            Console.Write("Input the number of the function you want to select : ");
             string? option = Console.ReadLine();
 
             // Function Option
@@ -34,7 +35,7 @@ class Program
                     Console.WriteLine("Welcome to Function Even number and Odd Number.");
                     while (keepRunningOption)
                     {
-                        Console.Write("Input your number: ");
+                        Console.Write("Input your number : ");
                         string? number = Console.ReadLine();
                         if (number == "exit")
                         {
@@ -47,6 +48,28 @@ class Program
                         else
                         {
                             Functions.funcEvenNumberAndOddNumber(number);
+                        }
+                    }
+                    break;
+                case "2":
+                    bool keepRunningOption2 = true;
+                    while (keepRunningOption2)
+                    {
+                        Console.Write("Input Random Length : ");
+                        string? lengthRd = Console.ReadLine();
+                        Console.Write("Input Random Count : ");
+                        string? countRd = Console.ReadLine();
+                        if(lengthRd == "exit" || countRd == "exit")
+                        {
+                            keepRunningOption2 = false;
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.WriteLine("Exiting to main menu.");
+                            Console.ResetColor();
+                            Console.WriteLine(line);
+                        }
+                        else
+                        {
+                            Functions.funcRandom(lengthRd, countRd);
                         }
                     }
                     break;
@@ -90,5 +113,29 @@ public class Functions
         }
 
         Console.ResetColor();
+    }
+
+    public static void funcRandom(string? randomCount, string? randomLength)
+    {
+        // Try to parse the input strings to integers
+        if (int.TryParse(randomCount, out int count) && int.TryParse(randomLength, out int length))
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < count; i++)
+            {
+                string randomNumber = "";
+                for (int j = 0; j < length; j++)
+                {
+                    // Generate a random digit between 0 and 9
+                    randomNumber += random.Next(0, 10);
+                }
+                Console.WriteLine(randomNumber);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter valid numbers for both the count and length.");
+        }
     }
 }
--- TestAndLearningConsoleApp/obj/Debug/net8.0/TestAndLearningConsoleApp.AssemblyInfo.cs
+++ TestAndLearningConsoleApp/obj/Debug/net8.0/TestAndLearningConsoleApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestAndLearningConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29398e5b75b3255f07f5a217184c51b9612c1558")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0a2930ac04fd18e65d1989f4df968ebd58dceee")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestAndLearningConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestAndLearningConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
