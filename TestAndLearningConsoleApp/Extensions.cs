d9502612aa76237a774f6896f4277d1abd6eb4af
BankVisaruts <visarut.ik@gmail.com>
2024-08-06 20:48:23 +0700

    Edit Extension

--- TestAndLearningConsoleApp/Extensions.cs
+++ TestAndLearningConsoleApp/Extensions.cs
@@ -10,17 +10,23 @@ namespace TestAndLearningConsoleApp
     {
         public static void consoleWriteLineGreen(string? text)
         {
-
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(text);
+            Console.ResetColor();
         }
 
         public static void consoleWriteLineRed(string? text)
         {
-
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(text);
+            Console.ResetColor();
         }
 
         public static void consoleWriteLineBlue(string? text)
         {
-
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(text);
+            Console.ResetColor();
         }
     }
 }
--- TestAndLearningConsoleApp/obj/Debug/net8.0/TestAndLearningConsoleApp.AssemblyInfo.cs
+++ TestAndLearningConsoleApp/obj/Debug/net8.0/TestAndLearningConsoleApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestAndLearningConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a84e673e7490bf13d7449268dc45f4537998a4b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ab208c8a39d34dbd2e7879a45cb12a0b7907d98")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestAndLearningConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestAndLearningConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
