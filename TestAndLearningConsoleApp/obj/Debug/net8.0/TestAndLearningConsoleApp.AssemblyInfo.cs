7c89b0045f76810da6b2c9ab0086c51a094e254b
BankVisaruts <visarut.ik@gmail.com>
2024-08-12 10:57:53 +0700

    edit func

--- TestAndLearningConsoleApp/obj/Debug/net8.0/TestAndLearningConsoleApp.AssemblyInfo.cs
+++ TestAndLearningConsoleApp/obj/Debug/net8.0/TestAndLearningConsoleApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestAndLearningConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79e8936932ad9476b46a8f7481b27f0b2e6783be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c9897bf1f35bf8fa4ca7266c1c04fe967d8ee4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestAndLearningConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestAndLearningConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
